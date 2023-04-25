d8591683685fbeb4802b200133496886efae2995
edgars <edgarpetros@gmail.com>
2023-04-25 13:42:25 -0700

    tasks

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+ * Description: macro named defined header file
+ * SIZE as an abbr for token 1024
+ */
+
+#define SIZE 1024
+
+#endif
